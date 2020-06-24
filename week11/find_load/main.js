fad41eed38bbc05069c4ed4f816fb882c5c669eb
bakenray <lpqstrong@163.com>
2020-06-24 21:23:40 +0800

    二叉堆实现

--- week11/find_load/main.js
+++ week11/find_load/main.js
@@ -1,3 +1,55 @@
+class BinaryHeap {
+    constructor(data,compare){
+        this.data = data
+        this.compare = compare || ((a,b) => a - b)
+    }
+    take(){
+        if(!this.data.length)
+            return;
+        let min = this.data[0]
+        let i = 0;
+        while(i<this.data.length){
+            if(i * 2 + 1 >= this.data.length)
+                break;
+            if(i * 2 + 2 >= this.data.length){
+                this.data[i] = this.data[i * 2 + 1]
+                i = i * 2 + 1
+                break;
+            }
+
+            if(this.compare(this.data[i * 2 + 1],this.data[i * 2 + 2]) < 0){
+                this.data[i] = this.data[i * 2 + 1]
+                i = i*2 + 1
+            }
+            else{
+                this.data[i] = this.data[i * 2 + 2]
+                i = i*2 + 2
+            }
+        }
+        if(i < this.data.length - 1){
+            this.insertAt(i,this.data.pop())
+        }
+        else{
+            this.data.pop()
+        }
+        return min
+    }
+    insertAt(i,v){
+        this.data[i] = v
+        
+        while(i > 0 && this.compare(v,this.data[Math.floor((i - 1)/2)])<0){
+            this.data[i] = this.data[Math.floor((i - 1)/2)]
+            this.data[Math.floor((i - 1)/2)] = v
+            i = Math.floor((i - 1)/2)
+        }
+    }
+    insert(v){
+        this.insertAt(this.data.length,v)
+    }
+    get length(){
+        return this.data.length
+    }
+}
 
 class Sorted {
     constructor(data,compare){
@@ -79,7 +131,7 @@ async function findPath(map,start,end){
         return (x - end[0]) ** 2 + (y - end[1]) ** 2;
     }
 
-    let collection = new Sorted([start], (a,b) => distance(a) - distance(b))
+    let collection = new BinaryHeap([start], (a,b) => distance(a) - distance(b))
     
     container.children[start[1] * 100 + start[0]].style.backgroundColor = 'green'
     container.children[end[1] * 100 + end[0]].style.backgroundColor = 'red'
