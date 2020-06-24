03a2356bee245da638ffe6ad983a7379f1a72001
bakenray <lpqstrong@163.com>
2020-06-24 16:29:20 +0800

    完成基本寻路

--- week11/find_load/main.js
+++ week11/find_load/main.js
@@ -4,7 +4,6 @@ let mouse = false;
 let clear = false;
 let container = document.getElementById('container');
 
-
 for(let y = 0; y < 100; y++){
     for(let x = 0; x < 100; x++){
         let cell = document.createElement('div');
@@ -27,11 +26,52 @@ for(let y = 0; y < 100; y++){
         container.appendChild(cell);
     }
 }
+events()
+findPath(map,[0,0],[50,50])
 
 
-document.addEventListener('mousedown',(e) => {
-    mouse = true
-    clear = (e.which === 3)
-});
-document.addEventListener('mouseup',() => mouse = false);
-document.addEventListener('contextmenu',e => e.preventDefault());
+function events(){
+    document.addEventListener('mousedown',(e) => {
+        mouse = true
+        clear = (e.which === 3)
+    });
+    document.addEventListener('mouseup',() => mouse = false);
+    document.addEventListener('contextmenu',e => e.preventDefault());
+}
+
+async function findPath(map,start,end){
+    map = map.slice()
+
+    let queue = [start]
+
+    async function insert([x,y]){
+        if(map[100 * y + x] !==0){
+            return
+        }
+        if(x < 0 || y < 0 || x >= 100 || y >=100){
+            return
+        }
+        map[100 * y + x] = 2
+        container.children[y * 100 + x].style.backgroundColor = 'lightgreen'
+        await sleep(5)
+        queue.push([x,y])
+    }
+
+    while(queue.length){
+        let [x,y] = queue.shift()
+        if(x === end[0] && y === end[1])
+            return true
+        await insert([x - 1,y])
+        await insert([x + 1,y])
+        await insert([x,y - 1])
+        await insert([x,y + 1])
+    }
+    return false
+}
+
+function sleep(t){
+    return new Promise(function(resolve){
+        setTimeout(resolve,t)
+    })
+}
+
