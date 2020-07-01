37803fd1aa4fda4d11ce153ab20d5675c915dd52
bakenray <lpqstrong@163.com>
2020-07-01 10:35:47 +0800

    处理正则没匹配全的bug

--- week12/AST.js
+++ week12/AST.js
@@ -1,49 +1,49 @@
-// var regexp = /([0-9\.]+)|([ ]+)|([\r\n]+)|(\+)|(\-)|(\*)(\/)/g
+var regexp = /([0-9\.]+)|([ ]+)|([\r\n]+)|(\+)|(\-)|(\*)|(\/)/g
 
-// var dictionary = ['Number','Whitespace','LineTerminator','+','-','*','/']
+var dictionary = ['Number','Whitespace','LineTerminator','+','-','*','/']
 
-// function* tokenize(source){
-//     var result = null;
-//     var lastIndex = 0;
+function* tokenize(source){
+    var result = null;
+    var lastIndex = 0;
 
-//     while(true) {
-//         lastIndex = regexp.lastIndex
-//         result = regexp.exec(source)
-//         console.log('result',result)
-//         if(!result) break;
+    while(true) {
+        lastIndex = regexp.lastIndex
+        result = regexp.exec(source)
+        if(!result) break;
 
-//         // if(regexp.lastIndex - lastIndex > result[0].length)
-//         //     throw new Error('Unexpected token \'' + source.slice(lastIndex,regexp.lastIndex - result[0].length) + '\'')
+        if(regexp.lastIndex - lastIndex > result[0].length)
+            throw new Error('Unexpected token \'' + source.slice(lastIndex,regexp.lastIndex - result[0].length) + '\'')
 
-//         let token = {
-//             type:null,
-//             value:null
-//         }
+        let token = {
+            type:null,
+            value:null
+        }
 
-//         for(var i = 0; i < dictionary.length; i++){
-//             if(result[i]){
-//                 token.type = (dictionary[i])
-//             }           
-//         }
-//         token.value = (result[0]) 
-//         yield  token;
-//     }
-// }
+        for(var i = 0; i < dictionary.length; i++){
+            if(result[i+1]){
+                token.type = (dictionary[i])
+            }           
+        }
+        token.value = (result[0]) 
+        yield  token;
+    }
+    yield {type:"EOF"}
+}
 
-// // function Expression(tokens){
+function Expression(tokens){
     
-// // }
-// // function AdditiveExpression(source){
+}
+function AdditiveExpression(source){
     
-// // }
-// // function MultiplicativeExpression(source){
-// //     console.log(source)
-// // }
-// // let source = []
+}
+// 连乘
+function MultiplicativeExpression(source){
+    console.log(source)
+}
+let source = []
 
-// for(let token of tokenize("1024 + 10 * 25")){
-//     console.log(token)
-//     // if(token.type!== 'Whitespace' && token.type !== 'LineTerminator')
-//     // source.push(token)
-// }
-// // MultiplicativeExpression(source)
+for(let token of tokenize("1024 + 10 * 25")){
+    if(token.type!== 'Whitespace' && token.type !== 'LineTerminator')
+    source.push(token)
+}
+MultiplicativeExpression(source)
