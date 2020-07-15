2267002544d3b7e1d897842d738c8d58a393d87a
bakenray <lpqstrong@163.com>
2020-07-15 18:00:13 +0800

    添加鼠标拖拽

--- week14/carousel_component/main.js
+++ week14/carousel_component/main.js
@@ -46,9 +46,58 @@ class Carousel{
 
       setTimeout(nextPic,3000);
     }
-    setTimeout(nextPic,3000)
+    // setTimeout(nextPic,3000)
 
+    root.addEventListener('mousedown',event => {
+      let startX = event.clientX,startY = event.clientY
 
+      let nextPosition = (position + 1)%this.data.length
+      let lastPosition = (position - 1 + this.data.length)%this.data.length
+
+      let current = children[position]
+      let last = children[lastPosition]
+      let next = children[nextPosition]
+
+      current.style.transition = 'ease 0s'
+      last.style.transition = 'ease 0s'
+      next.style.transition = 'ease 0s'
+
+      current.style.transform =`translateX(${- 500 * position}px)`
+      last.style.transform =`translateX(${- 500 - 500 * lastPosition}px)`
+      next.style.transform =`translateX(${500 - 500 * nextPosition}px)`
+
+      let move = event =>{         
+        current.style.transform =`translateX(${event.clientX - startX - 500 * position}px)`
+        last.style.transform =`translateX(${event.clientX - startX - 500 - 500 * lastPosition}px)`
+        next.style.transform =`translateX(${event.clientX - startX + 500 - 500 * nextPosition}px)`
+      }
+
+      let up = event =>{   
+        let offset = 0;        
+
+        if(event.clientX - startX > 250){
+          offset = 1
+
+        }else if(event.clientX  - startX < -250){
+          offset = -1
+        }
+
+        current.style.transition = 'ease .5s'
+        last.style.transition = 'ease .5s'
+        next.style.transition = 'ease .5s'
+
+        current.style.transform =`translateX(${offset * 500 - 500 * position}px)`
+        last.style.transform =`translateX(${offset * 500 - 500 - 500 * lastPosition}px)`
+        next.style.transform =`translateX(${offset * 500 + 500 - 500 * nextPosition}px)`
+
+        position = (position - 1 + offset + this.data.length)%this.data.length
+
+        document.removeEventListener('mousemove',move)
+        document.removeEventListener('mouseup',up)
+      }
+      document.addEventListener('mousemove',move)
+      document.addEventListener('mouseup',up)
+    })
     return root
   }
 
