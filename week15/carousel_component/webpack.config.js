83ef46baef4be9e1aeb009e198a2f61c463c84ec
bakenray <lpqstrong@163.com>
2020-07-22 15:22:10 +0800

    SFC

---
+++ week15/carousel_component/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports ={
+  mode:'development',
+  entry:'./main.js',
+  module:{
+    rules:[
+      {
+        test:/\.js$/,
+        use:{
+          loader:'babel-loader',
+          options:{
+            presets:['@babel/preset-env'],
+            plugins:[["@babel/plugin-transform-react-jsx",{pragma:'create'}]]
+          }
+        }
+      },
+      {
+        test:/\.ray/,
+        use:{
+          loader:require.resolve('./myloader.js')
+        }
+      }
+    ]
+  },
+  optimization:{
+    minimize:false
+  }
+}
