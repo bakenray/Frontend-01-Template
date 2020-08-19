616dc50dc317374cc2d18fd65aba09931cdb4305
bakenray <lpqstrong@163.com>
2020-08-19 22:55:53 +0800

    压缩

--- week19/publish-tool/publish.js
+++ week19/publish-tool/publish.js
@@ -1,39 +1,59 @@
 const http = require('http')
 const qs = require('querystring')
 const fs = require('fs')
+const archiver = require('archiver')
 
 const postData = qs.stringify({
   'content':'Hello Node.js!'
 })
 
-let filename = 'bg.jpg'
+let packname = './package'
 
-fs.stat('./'+filename,(err,stat)=>{
-
+// fs.stat('./'+filename,(err,stat)=>{
+// const req = http.request(options,(res)=>{})
   const options = { 
     host:'localhost',
     port:8081,
-    path:'/?filename=' + filename,
+    path:'/?filename=' + 'package.zip',
     method:'POST',
     headers:{
-      'Content-Type':'application/octet-stream',
-      'Content-Length':stat.size
+      'Content-Type':'application/octet-stream'
     }
   }
 
-  const req = http.request(options,(res)=>{})
-
+  const req = http.request(options,(res)=>{
+    
+  })
   req.on('error',(e)=>{
     console.log('problem width request',e.message)
   })
-  let readStream = fs.createReadStream('./'+filename)
-  readStream.pipe(req)
 
-  readStream.on('end',()=>{
+  var archive = archiver('zip',{
+    zlib:{level:9}
+  })
+
+  archive.directory(packname,false)
+  archive.pipe(req)
+  archive.finalize()
+  archive.on('end',()=>{
     req.end()
   })
 
-})
+
+
+  // const req = http.request(options,(res)=>{})
+  // req.on('error',(e)=>{
+  //   console.log('problem width request',e.message)
+  // })
+  // let readStream = fs.createReadStream('./'+filename)
+
+  // readStream.pipe(req)
+  
+  // readStream.on('end',()=>{
+  //   req.end()
+  // })
+
+// })
  
 
 
