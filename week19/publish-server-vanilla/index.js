f2a2d95244323ec3b726431b77c30a61fadae1a7
bakenray <lpqstrong@163.com>
2020-08-19 23:09:03 +0800

    解压

--- week19/publish-server-vanilla/index.js
+++ week19/publish-server-vanilla/index.js
@@ -1,15 +1,13 @@
 const http = require('http')
 const fs = require('fs')
+const unzipper = require('unzipper')
 
 const server = http.createServer((req,res)=>{
-    let matched = req.url.match(/filename=([^&]+)/)
-    let filename = matched && matched[1]
-    
-    if(!filename)
-      return
 
-    let writeStream = fs.createWriteStream('../server/public/' + filename)
+    let writeStream = unzipper.Extract({path:'../server/public'})
+
     req.pipe(writeStream)
+
     req.on('end',()=>{
       res.writeHead(200,{'Content-Type':'text/plain'})
       res.end('okay')
